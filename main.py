0341e639a8a8d2314d39ad0a736825a2462e7cd1
JansenSzeto <66149797+JansenSzeto@users.noreply.github.com>
2020-09-13 21:30:31 +0800

    commit

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import random
+
+def show():
+    print(answer)
+    print(len(answer))
+    j = []
+    for i in range(len(answer)):
+        j.append("_ ")
+    print("".join(j))
+
+def start():
+    f = open("words.txt", "r").readlines()
+    k = list(f[random.randrange(0, 497)])
+    del k[-1]
+    global answer
+    answer = "".join(k)
+    show()
+
+if __name__ == "__main__":
+    start()
