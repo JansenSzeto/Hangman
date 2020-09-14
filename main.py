fd1f6dee6863c5a1fbbab67d17886cb0b02466f1
JansenSzeto <66149797+JansenSzeto@users.noreply.github.com>
2020-09-14 10:08:20 +0800

    commit

--- main.py
+++ main.py
@@ -1,12 +1,82 @@
 import random
+import os
+
+j = []
+got = 0
+loss = 0
+
+man = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
 def show():
+    global j
+    global got
+    global loss
+    os.system('cls')
     print(answer)
     print(len(answer))
-    j = []
-    for i in range(len(answer)):
-        j.append("_ ")
+    print(man[loss])
     print("".join(j))
+    input_ = input()
+    if len(input_) == 1 and input_.isalpha() == True:
+        if input_ in list(answer):
+            for x in range(len(answer)):
+                if input_ == list(answer)[x]:
+                    j[x] = f"""{input_} """
+                    got += 1
+        else:
+            loss += 1
+    show()
+    
+
 
 def start():
     f = open("words.txt", "r").readlines()
@@ -14,6 +84,9 @@ def start():
     del k[-1]
     global answer
     answer = "".join(k)
+    global j
+    for i in range(len(answer)):
+        j.append("_ ")
     show()
 
 if __name__ == "__main__":
